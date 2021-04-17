ecae5d4c1c74a318915f2bd1815b37b1b90667ef
dimitrios vassilas <dimitrios.vassilas@fdmgroup.com>
2021-04-17 16:02:47 +0300

    Added routing to main app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Lists from './components/Lists';
+import NewReleases from './components/NewReleases';
+import Reviews from './components/Reviews';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container">
-      <Nav />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/lists" component={Lists} />
+          <Route path="/new-releases" component={NewReleases} />
+          <Route path="/reviews" component={Reviews} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
