a5cf1f9957d73cafa0bed4aa7e4b22f5b18fe17f
dimitrios vassilas <dmailvass96@gmail.com>
2021-10-12 19:47:58 +0100

    Added styling and mapping of reviews to Review page

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,18 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { apiRequest } from '../services/userServices';
 
 const Reviews = () => {
+
+  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     fetchItems();
   },[]);
 
   const fetchItems = async () => {
-    const data = await apiRequest('GET', 'http://localhost:8000/api/v1/articles/');
-  }
+        const response = await apiRequest('GET', 'http://localhost:8000/api/v1/articles/');
+        console.log(response);
+        const reviews = response.results
+        console.log(reviews);
+        setReviews(reviews);
+        console.log(reviews);
+    }
+
+  const reviewItems = reviews.map(review =>
+      <div className="review">
+        <a className="review_link">
+          <div className="review_title">
+            {review.title}
+          </div>
+          <div className="review_meta">
+            <ul>
+              <li>
+                By: {review.author}
+              </li>
+              <li>
+                Rating: {review.rating}
+              </li>
+            </ul>
+          </div>
+        </a>
+      </div>
+      );
 
   return (
-    <h2> Reviews </h2>
+    <div>
+      <h2> Reviews </h2>
+      <div className="reviews">
+        {reviewItems}
+      </div>
+    </div>
   )
 }
 
