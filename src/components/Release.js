7d25809233ef85d81bcf3919037e33126762a91a
dimitrios vassilas <dmailvass96@gmail.com>
2021-10-22 22:50:07 +0100

    Added in links to reviews for releases

--- src/components/Release.js
+++ src/components/Release.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Disc } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { apiRequest } from '../services/userServices';
 
 
 const NewRelease = () => {
   const [release, setRelease] = useState([]);
+  const [articleReviews, setArticleReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -17,8 +19,29 @@ const NewRelease = () => {
     const id = window.location.pathname.split('/').pop();
     const release = await apiRequest('GET', `http://localhost:8000/api/v1/releases/${id}`);
     setRelease(release);
+    setArticleReviews(release.articles)
     console.log(release)
   };
+
+  const articleReviewItems = articleReviews.map(review =>
+      <div className="release-article-reviews-item">
+        <Link to={`/reviews/${review.id}`}>
+          <div className="reviews-item-title">
+            {review.title}
+          </div>
+          <div className="reviews-item-meta">
+            <ul>
+              <li>
+                By: {review.author}
+              </li>
+              <li>
+                Rating: {review.rating}
+              </li>
+            </ul>
+          </div>
+        </Link>
+      </div>
+      );
 
   return (
       <div className="release">
@@ -32,6 +55,9 @@ const NewRelease = () => {
               <div className="release-artists"> {release.artists} </div>
             </li>
             <li>
+              <div className="release-type"> {release.type} </div>
+            </li>
+            <li>
               <div className="release-genre"> {release.genre} </div>
             </li>
             <li>
@@ -41,8 +67,12 @@ const NewRelease = () => {
         </div>
         <div className="release-reviews">
           <h2 className="release-sub-headers">Reviews</h2>
-          <div className="articles"></div>
-          <div className="user-reviews"></div>
+          <div className="articles">
+              {articleReviewItems}
+          </div>
+          <div className="user-reviews">
+            User Reviews
+          </div>
         </div>
         <div className="other-releases">
           <h2 className="release-sub-headers">Releases by this Artist</h2>
